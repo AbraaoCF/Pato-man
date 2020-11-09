4857ac36a403465284043c3d029250e8c24edf57
augustonz <augustonz2010@hotmail.com>
2020-11-09 12:47:22 -0300

    Criada classe Player

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-#Tile Size (tamanho de cada quadrado)
-TS = 16 
-
+#Initializing modules
+pygame.init()
 pygame.mixer.init()
-begin = pygame.mixer.Sound('sounds/begin.wav')
-begin.play()
-#Getting background img
-background = pygame.image.load('imgs/background'+str(TS)+'.png')
+
+#Getting images
+background=pygame.image.load('imgs/background16.png')
+
+#Creating the sounds
+begin=pygame.mixer.Sound('sounds/begin.wav')
 
 matriz         = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -54,32 +55,57 @@ RIGHT = 1
 DOWN  = 2
 LEFT  = 3
 
+#Tile Size (Define the number of pixels on each tile)
+TS = 16
+
+class Player:
+
+    def __init__(self):
+        self.pos=(14*TS,26*TS)
+        self.img=pygame.Surface((TS,TS))
+        self.img.fill((255,255,0))
+
+    def move(self,val):
+        if val == UP:
+            self.pos=(self.pos[0],self.pos[1]-TS)
+        if val == RIGHT:
+            self.pos=(self.pos[0]+TS,self.pos[1])
+        if val == DOWN:
+            self.pos=(self.pos[0],self.pos[1]+TS)
+        if val == LEFT:
+            self.pos=(self.pos[0]-TS,self.pos[1])
+
+    def move_absolute(self,x,y):
+        self.pos=(x,y)
+
+player=Player()
+
+#Tile Size (Define the number of pixels on each tile)
+TS = 16
+
 # Screen
 screen = pygame.display.set_mode((TS * 28, TS * 36))
 pygame.display.set_caption("Pato-man")
 
 # Set Pacman object and image
-pacman_obj = (TS, TS * 4);
 pacman_img = pygame.Surface((TS,TS))
-pacman_img.fill((255, 255, 0))
+pacman_img.fill((255,255,0))
 
 # Start Direction
-my_direction = RIGHT
+my_direction = LEFT
 
 # Clock Speed
 clock = pygame.time.Clock()
 
-def change_direction(direction):
-    global pacman_obj
-	
-    if direction ==    UP:
-        pacman_obj = (pacman_obj[0], pacman_obj[1] - TS)
-    if direction == RIGHT:
-        pacman_obj = (pacman_obj[0] + TS, pacman_obj[1])
-    if direction ==  DOWN:
-        pacman_obj = (pacman_obj[0], pacman_obj[1] + TS)
-    if direction ==  LEFT:
-        pacman_obj = (pacman_obj[0] - TS, pacman_obj[1])
+# Inital screen appearence
+screen.fill((0, 0, 0))
+screen.blit(background,(0,3*TS));
+screen.blit(player.img,player.pos)
+pygame.display.update()
+
+#plays the beginning sound
+begin.play()
+pygame.time.wait(4000);
 
 running = True
 while running:
@@ -101,32 +127,26 @@ while running:
             if event.key == K_RIGHT:
                 my_direction = RIGHT
 
-    # Directions update
-    change_direction(my_direction)
+    #Handles player movement
+    player.move(my_direction)
 
-    if pacman_obj[1] == TS*17:
-        if pacman_obj[0] < 0: pacman_obj = (screen.get_width() - TS, pacman_obj[1])
-        elif pacman_obj[0] >= screen.get_width(): pacman_obj = (0 - TS, pacman_obj[1])
-    #end of screen (Pacman para ao chegar na borda da tela)
-    
-    if pacman_obj[0] < 0: pacman_obj = (0,pacman_obj[1])
-    if pacman_obj[1] < 0: pacman_obj = (pacman_obj[0],0)
-    if pacman_obj[0] >= screen.get_width() -  TS: pacman_obj = (screen.get_width()-TS,  pacman_obj[1])
-    if pacman_obj[1] >= screen.get_height() - TS: pacman_obj = (pacman_obj[0], screen.get_height()-TS)
-    if (matriz[pacman_obj[1] // TS][pacman_obj[0] // TS] == 1):
+    if player.pos[1] == TS*17:
+        if player.pos[0] < 0: player.pos = (screen.get_width() - TS, player.pos[1])
+        elif player.pos[0] >= screen.get_width(): player.pos = (0 - TS, player.pos[1])
+
+    #Makes Pac-man stop on walls
+    if (matriz[player.pos[1] // TS][player.pos[0] // TS] == 1):
 	    if my_direction ==    UP:
-		    pacman_obj = (pacman_obj[0], pacman_obj[1] + TS)
+		    player.pos = (player.pos[0], player.pos[1] + TS)
 	    if my_direction == RIGHT:
-		    pacman_obj = (pacman_obj[0] - TS, pacman_obj[1])
+		    player.pos = (player.pos[0] - TS, player.pos[1])
 	    if my_direction ==  DOWN:
-		    pacman_obj = (pacman_obj[0], pacman_obj[1] - TS)
+		    player.pos = (player.pos[0], player.pos[1] - TS)
 	    if my_direction ==  LEFT:
-		    pacman_obj = (pacman_obj[0] + TS, pacman_obj[1])
+		    player.pos = (player.pos[0] + TS, player.pos[1])
 	
-    # Screen
+    # Objects on screen
     screen.fill((0, 0, 0))
-    
-    # Object on screen
-    screen.blit(background,(0, 3 * TS));
-    screen.blit(pacman_img, pacman_obj)
+    screen.blit(background,(0,3*TS))
+    screen.blit(player.img,player.pos)
     pygame.display.update()
