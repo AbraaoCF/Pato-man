d7cf0dcc6b48566c823af13e833c17b3a8e3ed72
AbraaoCF <abraao.freitas@cc.ufcg.edu.br>
2020-11-16 22:04:42 -0300

    cursor and menu options

--- Patoman/menu.py
+++ Patoman/menu.py
@@ -9,6 +9,13 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 #top: TS*32 bottom: TS*66
 #left: TS*4 right: TS*50
 
+#Directions
+UP    = 0
+RIGHT = 1
+DOWN  = 2
+LEFT  = 3
+STOP  = 4
+
 def load_img(name):
    path=os.path.join(main_dir,"imgs",name)
    return pygame.image.load(path)
@@ -23,6 +30,10 @@ def load_font(name):
    path=os.path.join(main_dir,"fonts",name)
    return pygame.font.Font(path,16)
 
+def load_font_menu(name):
+   path=os.path.join(main_dir,"fonts",name)
+   return pygame.font.Font(path,28)
+
 #loading resources
 patoFC = load_img('patoFC.png')
 patoFD = load_img('patoFD.png')
@@ -34,7 +45,7 @@ patoAB = load_img('patoAB.png')
 patoAE = load_img('patoAE.png')
 
 font=load_font("emulogic.ttf")
-
+font_menu=load_font_menu("emulogic.ttf")
 TS=8 #setting the size in pixels of a tile
 
 screen = pygame.display.set_mode((TS * 56, TS * 72)) #sets screen size
@@ -55,13 +66,60 @@ class Header():
         screen.blit(font.render("          -Esc to pause",False,pygame.Color('White')),(16,208))
         screen.blit(self.title,(81,64)) #shows title image
 
+class Main_Menu():
+
+    def __init__(self):
+        pass
+
+    def display(self):
+        screen.blit(font_menu.render("Start", False, pygame.Color('White')),(82,288))
+        screen.blit(font_menu.render("Settings", False, pygame.Color('White')),(82,322))
+        screen.blit(font_menu.render("Credits", False, pygame.Color('White')),(82,358))
+        screen.blit(font_menu.render("Quit", False, pygame.Color('White')),(82,394))
+
+class Cursor():
+    
+    def __init__(self):
+        self.arrow = load_img('right_arrow.png')
+        self.start = (62,296) #Cursor pointing to 'start' 
+        self.settings = (62,330) #Cursor pointing to 'settings'
+        self.credits = (62,366) #Cursor pointing to 'credits'
+        self.quit = (62,402) #Cursor pointing to 'quit'
+        self.pos = self.start
+        self.direction = STOP
+
+    def move(self):
+        if self.direction == STOP or self.direction == RIGHT or self.direction == LEFT:
+            pass
+        elif self.direction == UP:
+            if self.pos == self.start:
+                self.pos = self.quit
+            elif self.pos == self.settings:
+                self.pos = self.start
+            elif self.pos == self.credits:
+                self.pos = self.settings
+            elif self.pos == self.quit:
+                self.pos = self.credits
+        elif self.direction == DOWN:
+            if self.pos == self.quit:
+                self.pos = self.start
+            elif self.pos == self.start:
+                self.pos = self.settings
+            elif self.pos == self.settings:
+                self.pos = self.credits
+            elif self.pos == self.credits:
+                self.pos = self.quit
+        self.direction = STOP
+
+    def display(self):
+        screen.blit(self.arrow,self.pos)
+
 class Animation():
 
     def __init__(self):
         self.pos=(TS*1,TS*29) #starting postion
         self.img=patoAD #starting image
         self.aberto=True #Variable to make mouth open and close
-
 
     def animate(self):
         self.aberto=not self.aberto 
@@ -85,6 +143,8 @@ class Animation():
 #creating classes        
 header=Header()
 animation=Animation()
+menu=Main_Menu()
+cursor=Cursor()
 
 while True:
     clock.tick(15)
@@ -97,20 +157,18 @@ while True:
             #Directions keys
             if event.type == KEYDOWN:
                 if event.key == K_UP:
-                    pass
-                    #player.direct = UP
+                    cursor.direction = UP
                 if event.key == K_DOWN:
-                    pass
-                    #player.direct = DOWN
+                    cursor.direction = DOWN
                 if event.key == K_LEFT:
-                    pass
-                    #player.direct = LEFT
+                    cursor.direction = LEFT
                 if event.key == K_RIGHT:
-                    pass
-                    #player.direct = RIGHT
-
+                    cursor.direction = RIGHT
+             
     screen.fill((0, 0, 0)) #Display objects on screen
-    header.display() #displays header
-    animation.animate() # shows duck animation
-
+    header.display() #Displays header
+    menu.display() #Displays menu
+    animation.animate() #Shows duck animation
+    cursor.move()#Moves cursor
+    cursor.display()#Displays cursor
     pygame.display.update()
